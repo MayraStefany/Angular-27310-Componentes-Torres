0d0ca6b7ff758dc15129aac08557f6f5eaea7533
Mayra Torres <isemayratorres18@gmail.com>
2022-03-20 19:37:08 -0500

    Avance Sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { StudentsComponent } from './components/students/students.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    StudentsComponent,
+    ToolbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
